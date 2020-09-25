1519afd53c2b4c3f455c94eab2d3f329b41d16af
iamkyu <kyujnam@gmail.com>
2020-09-25 23:37:12 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+    application
+}
+group = "com.namkyujin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
+
+    testImplementation(kotlin("test-junit5"))
+}
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+application {
+    mainClassName = "MainKt"
+}
